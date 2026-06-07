66e0d8d01144181236caf7ce28f773886369018e
Harini-SA <harinisa1109@gmail.com>
2026-06-07 21:51:19 +0530

    added a database

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "expenses.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 CORS(app)
+
 
 @app.route("/dashboard")
 def dashboard():
